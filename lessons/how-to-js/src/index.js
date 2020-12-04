e29401bbae9b28c154ebfbcfe8c6cc97acb949ca
Vladislav Bukatov <vasyayp@yandex.ru>
2020-12-04 13:44:13 +0400

    feat(how-to-js): initialization object literal and variables write to it via prompt
    affects: @lessons/how-to-js
    
    ISSUES CLOSED: #1

--- lessons/how-to-js/src/index.js
+++ lessons/how-to-js/src/index.js
@@ -1,1 +1,20 @@
-let copyright = 'place some bullshit here'
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+const q = prompt("Один из последних просмотренных фильмов?", ""),
+  w = prompt("Какую оценку вы бы ему поставили?", ""),
+  e = prompt("Один из последних просмотренных фильмов?", ""),
+  z = prompt("Какую оценку вы бы ему поставили?", "");
+
+personalMovieDB.movies[q] = w;
+personalMovieDB.movies[e] = z;
+
+console.log(personalMovieDB);
+
