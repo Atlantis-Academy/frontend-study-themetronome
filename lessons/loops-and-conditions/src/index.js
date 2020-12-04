cc5f95573086a11490bbb1c5a8a99ec7cde6c3a5
Vladislav Bukatov <vasyayp@yandex.ru>
2020-12-05 01:08:50 +0400

    feat(loops-and-conditions): add answer check from prompt using if/else condition
    affects: @lessons/loops-and-conditions
    
    ISSUES CLOSED: #3

--- lessons/loops-and-conditions/src/index.js
+++ lessons/loops-and-conditions/src/index.js
@@ -12,7 +12,11 @@ for (let i = 0; i < 2; i++) {
   const q = prompt("Один из последних просмотренных Вами фильмов?", "");
   const w = prompt("Какую оценку вы бы ему поставили?", "");
 
-  personalMovieDB.movies[q] = w;
+  if (q != null && w != null && q !== "" && w !== "" && q.length < 50) {
+    personalMovieDB.movies[q] = w;
+  } else {
+    i--;
+  }
 }
 
 console.log(personalMovieDB);
