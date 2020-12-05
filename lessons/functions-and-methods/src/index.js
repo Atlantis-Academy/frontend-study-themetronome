67d1ce9fedf55654acc40e15d3dcda02d26841ce
Vladislav Bukatov <vasyayp@yandex.ru>
2020-12-05 18:30:34 +0400

    refactor(@lessons/functions-and-methods): encapsulate an existing code into functions
    affects: @lessons/functions-and-methods
    
    ISSUES CLOSED: #5

---
+++ lessons/functions-and-methods/src/index.js
@@ -0,0 +1,50 @@
+let numberOfFilms;
+
+function initialAmountCheck() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+
+initialAmountCheck();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+function recordFilmsInformation() {
+  for (let i = 0; i < 2; i++) {
+    const q = prompt("Один из последних просмотренных Вами фильмов?", "");
+    const w = prompt("Какую оценку вы бы ему поставили?", "");
+
+    if (q != null && w != null && q !== "" && w !== "" && q.length < 50) {
+      personalMovieDB.movies[q] = w;
+    } else {
+      i--;
+    }
+  }
+}
+
+recordFilmsInformation();
+
+function consoleViewerLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Вы довольно редко смотрите фильмы");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы среднестатистический зритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Да вы киноман!");
+  } else {
+    console.log("Ошибка!");
+  }
+}
+
+consoleViewerLevel();
+
+console.log(personalMovieDB);
