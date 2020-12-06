3e5bea7ce0df47b85ff432ad8c6c9723d5808bce
Vladislav Bukatov <vasyayp@yandex.ru>
2020-12-06 15:05:06 +0400

    refactor(@lessons/functions-and-methods): encapsulation an existing instructions into functions
    affects: @lessons/functions-and-methods
    
    ISSUES CLOSED: #5

---
+++ lessons/functions-and-methods/src/index.js
@@ -0,0 +1,52 @@
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const totalWatchedMoviesCheck = () => {
+  while (
+    numberOfFilms === "" ||
+    numberOfFilms == null ||
+    isNaN(numberOfFilms)
+  ) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+};
+
+totalWatchedMoviesCheck();
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+function lastSeenMovieRate() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", "");
+    const b = prompt("Какую оценку вы бы ему поставили?", "");
+
+    if (a != null && b != null && a !== "" && b !== "" && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
+  }
+}
+
+lastSeenMovieRate();
+
+function viewerGrade() {
+  if (personalMovieDB.count < 10) {
+    console.log("Вы смотрите фильмы довольно редко");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Обычный кинозритель");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Да вы киноман!");
+  } else {
+    console.log("Ошибка!");
+  }
+}
+
+viewerGrade();
+
+console.log(personalMovieDB);
