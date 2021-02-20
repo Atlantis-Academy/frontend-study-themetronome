eb15a69887253ba6797eeabce91aa92b994be472
Vladislav Bukatov <vasyayp@yandex.ru>
2021-02-20 17:28:17 +0400

    refactor(legacy-to-esnext): refactor using newer ES features (#61)
    affects: @practice/legacy-to-esnext
    
    ISSUES CLOSED: #60

---
+++ practice/legacy-to-esnext/src/index.ts
@@ -0,0 +1,50 @@
+const employers: string[] = [
+  'Alex',
+  '',
+  'ludmila',
+  'Victor',
+  '',
+  'oleg',
+  'iNna',
+  'Ivan',
+  'Alex',
+  'Olga',
+  ' Ann',
+]
+
+const employersNames: string[] = []
+for (let i: number = 0; i < employers.length; i++) {
+  if (employers[i].length > 0 && employers[i] !== '') {
+    employersNames.push(employers[i])
+  }
+}
+for (let i: number = 0; i < employersNames.length; i++) {
+  employersNames[i] = employersNames[i].toLowerCase().trim()
+}
+
+const sponsors: { cash: number[]; eu: string[]; rus: string[] } = {
+  cash: [40000, 5000, 30400, 12000],
+  eu: ['SRL', 'PLO', 'J&K'],
+  rus: ['RusAuto', 'SBO'],
+}
+
+function calcCash(own = 0, ...everyCash: (string | number)[][]) {
+  let total = own
+  for (let i = 0; i < everyCash[1].length; i++) {
+    total += +everyCash[1][i]
+  }
+  return total
+}
+
+const money = calcCash(...sponsors.cash)
+
+function makeBusiness(owner: string, director: string = 'Victor', cash: number, emp: string[]) {
+  const sumSponsors = [...sponsors.eu, ...sponsors.rus, 'unexpected sponsor']
+  console.log(
+    `We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}`
+  )
+  console.log('And we have a sponsors: ')
+  console.log(...sumSponsors)
+  console.log(`Note. Be careful with ${sponsors.eu[0]}. It's a huge risk.`)
+}
+makeBusiness('Sam', undefined, money, employersNames)
